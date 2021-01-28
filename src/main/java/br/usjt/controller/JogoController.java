db564c4b90fffeea2e1163d77d51eb913c964ac3
Ismael de Oliveira <mael.jozi@gmail.com>
2021-01-27 23:13:47 -0300

    init

---
+++ src/main/java/br/usjt/controller/JogoController.java
@@ -0,0 +1,5 @@
+package br.usjt.controller;
+
+public class JogoController {
+
+}
---
+++ src/test/java/br/usjt/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.usjt.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
